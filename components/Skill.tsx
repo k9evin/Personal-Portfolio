9298332a6100a7a7705e158c0f1b9de68c4af166
Mingkai Pang <pangmingkai@gmail.com>
2022-12-30 10:13:17 -0800

    🦄 refactor(div): motion div refactor

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -7,17 +7,19 @@ type Props = {
 
 function Skill({ directionLeft }: Props) {
   return (
-    <div className='flex gap-2 items-center'>
-      <motion.img
-        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1.5 }}
+    <motion.div
+      initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex gap-2 items-center"
+    >
+      <img
         className="rounded-full h-10 w-10 inline-flex"
         src="https://cdn.worldvectorlogo.com/logos/react-2.svg"
         alt="React"
       />
       <span className="text-white text-lg">React</span>
-    </div>
+    </motion.div>
   );
 }
 
