a59228cd18f8e259726916d462e735c210f0ea8f
Mingkai Pang <pangmingkai@gmail.com>
2022-12-30 01:58:04 -0800

    ✨ feat(hero): Complete hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircle';
+import Image from 'next/image';
+import Link from 'next/link';
 
-type Props = {}
+type Props = {};
 
 function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: ['Hey, My name is Mingkai Pang', 'Guy-who-loves-coding.tsx'],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
   return (
-    <div>Hero</div>
-  )
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+
+      <Image
+        src="https://s1.ax1x.com/2022/12/30/pSpokTS.png"
+        width={180}
+        height={180}
+        className="relative rounded-full mx-auto object-cover"
+        alt="Kevin"
+      />
+      <div className='z-20'>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton hover:border-white">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Mingkai Pang</title>
       </Head>
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id='hero'>
+      <section id="hero" className='snap-center'>
         <Hero />
       </section>
 
@@ -28,6 +28,6 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact Me */}
-    </>
+    </div>
   );
 }
