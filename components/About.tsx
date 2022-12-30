a0b787eb3a08af660a4fb376c3cc0785591e17c0
Mingkai Pang <pangmingkai@gmail.com>
2022-12-30 02:36:08 -0800

    ✨ feat(about): Complete about section

---
+++ components/About.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        src="https://s1.ax1x.com/2022/12/30/pSpqvY8.png"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-24 h-24 rounded-full object-cover md:w-64 md:h-64 "
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{' '}
+          <span className="underline  decoration-[#F7AB0A] decoration-wavy">
+            litte
+          </span>{' '}
+          background
+        </h4>
+        <p className="text-base">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, alias
+          aut, harum totam non sapiente doloremque eum amet voluptas magni enim
+          animi a nisi, eligendi quisquam et. Obcaecati, quisquam id? A autem
+          soluta laudantium qui unde asperiores voluptatem repellat ratione nam
+          dolore corporis ipsum at, exercitationem ipsa eos doloremque, culpa
+          rerum saepe sed ex cumque. Omnis voluptate quas ea consequatur. Nobis
+          beatae sed labore similique ducimus, illum placeat assumenda quidem
+          cumque dolores in consectetur necessitatibus perspiciatis tempore
+          magnam ipsa.
+        </p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { Inter } from '@next/font/google';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,12 +15,13 @@ export default function Home() {
 
       <Header />
 
-      {/* Hero */}
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero />
       </section>
 
-      {/* About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
 
       {/* Experience */}
 
