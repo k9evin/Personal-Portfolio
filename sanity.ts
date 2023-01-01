f7f530287f8a4dc7ef2ed92299413ead3716b23e
Mingkai Pang <pangmingkai@gmail.com>
2023-01-01 06:46:21 -0800

    🦄 refactor(type): Type update

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuilder from '@sanity/image-url';
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
   useCdn: process.env.NODE_ENV === 'production',
   apiVersion: '2021-10-21',
 };
