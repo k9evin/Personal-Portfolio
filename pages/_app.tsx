4e19823dae9c957b79b67b001d0ac37dbbd02667
Mingkai Pang <pangmingkai@gmail.com>
2023-01-09 16:40:00 -0800

    ✨ feat(analytics): Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+        `}
+      </Script>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
