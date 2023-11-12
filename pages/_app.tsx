bb2499e4e16ba3631e975dda2a7bc9e664f91958
Mingkai Pang <pangmingkai@gmail.com>
2023-11-12 14:42:41 -0800

    :sparkles: add web analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 `}
       </Script>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
